aef8eabb825479545ea0f247fdf1a06d29250495
aaronbraganca2 <144013061+aaronbraganca2@users.noreply.github.com>
2023-11-03 23:03:36 +0530

    Add files via upload

---
+++ Snake/src/snake/SnakeGame.java
@@ -0,0 +1,10 @@
+package snake;
+
+public class SnakeGame {
+	
+	public static void main(String[] args) {
+		
+		new GameFrame();//no need to provide an identifier since we wont be using the game frame for anything else
+	}
+
+}
